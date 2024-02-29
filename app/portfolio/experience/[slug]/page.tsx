db33a4dcdd172aafa5621ea97e66934d30e84457
James Spillmann <james@spacinsider.com>
2024-02-29 02:20:04 -0500

    experience page done for now

--- app/portfolio/experience/[slug]/page.tsx
+++ app/portfolio/experience/[slug]/page.tsx
@@ -13,10 +13,9 @@ export default function ExperiencePage({
 }) {
   const experience = getExperienceByTitle(decodeURIComponent(params.slug));
   return (
-    <div className="flex justify-center items-center pt-20 px-4 min-h-screen">
+    <div className="flex justify-center items-center pt-20 px-4 ">
       {experience && (
         <div className="bg-white shadow-xl rounded-3xl overflow-hidden w-full max-w-4xl">
-          {/* Header or Hero Section */}
           <div className="bg-gray-200 text-center py-12">
             <h1 className="text-4xl font-bold text-gray-800">
               {experience.title}
@@ -26,32 +25,30 @@ export default function ExperiencePage({
             </p>
           </div>
 
-          {/* Detailed Text Section */}
           <div className="p-8">
             <p className="text-gray-700 text-lg whitespace-pre-line">
               {experience.long_text}
             </p>
           </div>
 
-          {/* Call to Action or Link */}
           {experience && (
-            <div className="bg-gray-200 px-8 py-4 h-20">
-              <Image
-                src="/images/github_mark.png"
-                alt="GitHub Logo"
-                width={80}
-                height={80}
-                layout="responsive"
-                style={{ borderRadius: '50%' }} // Optional: if you want a circular mask
-              />
-              {/* <a
+            <div className="bg-gray-200 px-8 h-16 max-h-full max-w-full hover:bg-gray-300">
+              <a
                 href={experience.link}
-                className="text-blue-500 hover:text-blue-700 transition duration-150 ease-in-out"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                GitHub
-              </a> */}
+                {' '}
+                {/* eslint-disable-line */}
+                <div className="flex justify-center items-center py-3">
+                  <Image
+                    src="/images/github_mark.png"
+                    alt="GitHub Logo"
+                    width={40}
+                    height={40}
+                  />
+                </div>
+              </a>
             </div>
           )}
         </div>
