d326f91e8b4c012692609607d8bc97d0b1b59e10
James Spillmann <james@spacinsider.com>
2024-03-04 21:44:28 -0500

    about formatting

--- app/portfolio/about/page.tsx
+++ app/portfolio/about/page.tsx
@@ -14,74 +14,74 @@ export default function About() {
         </div>
         <div className="p-6">
           {/* Image container floated to the right */}
-          <div className="relative rounded-xl bg-gray-200 overflow-hidden w-96 h-96 float-right ml-6 mb-4">
-            <Image
-              src="/images/james_portrait.png" // Replace with your photo path
-              alt="Profile Photo"
-              objectFit="cover"
-              className="rounded-xl"
-              fill
-            />
-          </div>
-          {/* Text content */}
-          <div className="flex justify-center">
-            <div className="w-fit h-fit bg-gray-200 p-2 rounded-xl font-bold">
-              Background
+          <div className="bg-gray-200 rounded-xl">
+            <div className="relative rounded-xl bg-gray-200 overflow-hidden w-96 h-96 float-right ml-6 mb-4">
+              <Image
+                src="/images/james_portrait.png" // Replace with your photo path
+                alt="Profile Photo"
+                objectFit="cover"
+                className="rounded-xl p-2"
+                fill
+              />
             </div>
+            {/* Text content */}
+            <div className="flex justify-center">
+              <div className="w-fit h-fit  p-2 font-bold">Background</div>
+            </div>
+            <p className="p-2 text-sm">
+              Hello! I&apos;m James, a Boulder, Colorado, native and a recent
+              graduate from the University of Michigan. Since packing up my bags
+              in Ann Arbor, I have settled into New York City to pursue a career
+              in the tech industry.
+              <br />
+              <br />
+              Upon arrival, with no job lined up (due to unforeseen
+              circumstances), I took my talents to the fine establishment of
+              Locanda Verde. After working with stars such as Robert Di Nero,
+              Ryan Reynolds, Frank Ocean, and many more, I decided to take my
+              talents back, to the tech industry.
+              <br />
+              <br />
+              Recently, at SPACInsider, my work has centered on crafting
+              flexible technological solutions, leveraging a wide range of
+              programming languages to boost user experiences and operational
+              efficiencies across platforms. I have played a key role in
+              bridging the gap between front-end design and back-end
+              architecture, enhancing my full-stack technical prowess.
+              <br />
+              <br />
+              As I continue to navigate the tech landscape, I&apos;m always on
+              the lookout for new opportunities and collaborations that
+              challenge and expand my skill set. Whether it&apos;s discussing
+              potential job openings, exploring innovative tech projects, or
+              simply exchanging ideas about the future of technology, please
+              feel free to reach out!
+            </p>
           </div>
-          <p className="pt-2 text-sm">
-            I am a passionate Computer Science and Cognitive Science graduate
-            from the University of Michigan. Armed with a robust Python, C++,
-            and JavaScript skill set, I&apos;ve applied my programming acumen in
-            diverse projects and roles. At Solidigm, I wrote and fixed Python
-            tests within an extensive codebase to ensure SSD functionality.
-            While at Intel, I specialized in firmware validation, adeptly
-            working with continuous integration software and crafting efficient
-            GUIs to boost performance. <br /> <br />
-            In my academic projects, I&apos;ve utilized MongoDB, Flask, and many
-            APIs to build applications, execute data transformations, and adopt
-            algorithms for efficient processes. One such project was the
-            &apos;TruBlu&apos; application, developed using Flask-Python, that
-            revolutionized college tours by fostering direct connections between
-            prospective and enrolled students with parallel interests in
-            academic majors and extracurricular activities. <br /> <br />
-            As a Cognitive Science major, I gained valuable insights into human
-            cognition, enabling me to create user-friendly applications and
-            solutions with an empathetic understanding of the user&apos;s
-            perspective. This knowledge equips me to work efficiently in UX/UI
-            design, Machine Learning, Artificial Intelligence, and more.
-            <br />
-            <br />I am keen on applying my diverse skills in software
-            engineering, web development, cybersecurity, fintech, and tech
-            consulting. My professional goal is to harness the power of
-            computing and human cognition to develop innovative solutions that
-            improve user experiences and business processes.
-            <br /> <br /> Feel free to connect with me for software engineering,
-            tech consulting, fintech, ML/AI, or web development opportunities.
-          </p>
           {/* Ensure the following content clears the float */}
-          <div className="clear-both">
-            <div>
+          <div className="flex w-full mt-4">
+            <div className="bg-gray-200 rounded-xl min-w-fit">
               <div className="flex justify-center">
                 <div className="w-fit h-fit bg-gray-200 p-2 rounded-xl font-bold">
                   Education
                 </div>
               </div>
-              <p className="pt-2 text-sm">
+              <p className="p-2 text-sm">
                 <strong>University of Michigan</strong> - Ann Arbor, MI
+                <br />
+                <span className="italic">August 2019 - May 2023</span>
                 <br />
                 Bachelor of Science in Computer Science and Cognitive Science
                 <br />
-                <strong>Graduated:</strong> May 2021
               </p>
             </div>
-            <div>
+            <div className="flex-grow bg-gray-200 ml-4 rounded-xl">
               <div className="flex justify-center">
                 <div className="w-fit h-fit bg-gray-200 p-2 rounded-xl font-bold">
                   Goals
                 </div>
               </div>
-              <p className="pt-2 text-sm">
+              <p className="p-2 text-sm">
                 I am keen on applying my diverse skills in software engineering,
                 web development, cybersecurity, fintech, and tech consulting. My
                 professional goal is to harness the power of computing and human
