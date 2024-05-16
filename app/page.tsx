3159fc90b1d5dac6eaf227b72e657e69fb0d1169
James Spillmann <james@spacinsider.com>
2024-05-16 11:23:34 -0400

    redirecting main page to portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 import RecPageItem from './components/pageItems/recPageItem';
+import { redirect } from 'next/navigation';
 
-export default function HomePage() {
-  return (
-    <div>
-      <div className="flex flex-col pt-20">
-        <div className="flex flex-col flex-grow p-4 m-4 lg:mx-80 bg-white shadow-lg rounded-2xl">
-          <RecPageItem bgColor="bg-red-500" />
-          <RecPageItem bgColor="bg-blue-500" />
-          <RecPageItem bgColor="bg-green-500" />
-          <RecPageItem bgColor="bg-yellow-400" />
-          <RecPageItem />
-        </div>
-      </div>
-    </div>
-  );
+export default async function HomePage() {
+  redirect('/portfolio');
+  // ...
 }
+
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <div className="flex flex-col pt-20">
+//         <div className="flex flex-col flex-grow p-4 m-4 lg:mx-80 bg-white shadow-lg rounded-2xl">
+//           <RecPageItem bgColor="bg-red-500" />
+//           <RecPageItem bgColor="bg-blue-500" />
+//           <RecPageItem bgColor="bg-green-500" />
+//           <RecPageItem bgColor="bg-yellow-400" />
+//           <RecPageItem />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
