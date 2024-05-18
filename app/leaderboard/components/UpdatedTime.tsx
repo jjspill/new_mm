c09c50af7a0894a68436540c379ef3298def80a2
James Spillmann <james@spacinsider.com>
2024-05-18 16:40:29 -0400

    memo timestamp

--- app/leaderboard/components/UpdatedTime.tsx
+++ app/leaderboard/components/UpdatedTime.tsx
@@ -1,10 +1,17 @@
 'use client';
 
+import React from 'react';
+
 const convertUTCDateToLocalDate = (date: string): string => {
   const newDate = new Date(date);
   return newDate.toLocaleString('en-US', { timeZoneName: 'short' });
 };
 
 export function UpdatedTime({ date }: { date: string }) {
-  return <span className="text-sm">{convertUTCDateToLocalDate(date)}</span>;
+  const convertedTime = React.useMemo(
+    () => convertUTCDateToLocalDate(date),
+    [date],
+  );
+
+  return <span className="text-sm">{convertedTime}</span>;
 }
