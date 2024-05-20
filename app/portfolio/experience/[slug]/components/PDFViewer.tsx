dc820be34e0ae8060b1a21c83e473537b9fbb363
James Spillmann <james@spacinsider.com>
2024-05-20 13:03:39 -0400

    working on pdf carousel mobile

--- app/portfolio/experience/[slug]/components/PDFViewer.tsx
+++ app/portfolio/experience/[slug]/components/PDFViewer.tsx
@@ -9,7 +9,7 @@ const PDFViewer: React.FC<PDFViewerProps> = ({ pdfFiles }) => {
   return (
     <div className="overflow-hidden p-4 pb-8">
       <div className="flex overflow-x-auto">
-        <div className="flex flex-row space-x-4 min-w-full items-start justify-start md:justify-center">
+        <div className="flex flex-row space-x-4 min-w-full items-start justify-start">
           {pdfFiles.map((file, index) => (
             <ClickableDiv key={index} path={file.path}>
               <div
