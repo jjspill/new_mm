261e42e51452dba3d3881c14764c8649ff8067f5
James Spillmann <james@spacinsider.com>
2024-05-21 23:32:41 -0400

    new backend

---
+++ app/portfolio/experience/experienceHelpers.ts
@@ -0,0 +1,56 @@
+export interface ExperienceProps {
+  title: string;
+  description: string;
+  long_text: string;
+  key_features: {
+    feature: string;
+    description: string;
+  }[];
+  youtube_video: string;
+  technologies: string[];
+  documents: {
+    title: string;
+    path: string;
+  }[];
+  link: string;
+}
+
+export async function getAllExperiences(): Promise<ExperienceProps[]> {
+  const res = await fetch(
+    'https://preview.api.james-spillmann.com/experiences',
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      next: { revalidate: 300 },
+    },
+  );
+
+  const experienceData = await res.json();
+
+  if (!experienceData?.data) {
+    console.error('Unable to all fetch data from the API.');
+  }
+
+  return experienceData?.data;
+}
+
+export async function getExperience(slug: string): Promise<ExperienceProps> {
+  const res = await fetch(
+    `https://preview.api.james-spillmann.com/experiences/${slug}`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      next: { revalidate: 0 },
+    },
+  );
+
+  const experienceData = await res.json();
+
+  if (!experienceData?.data) {
+    console.error('Unable to fetch data from the API.');
+  }
+
+  return experienceData?.data;
+}
